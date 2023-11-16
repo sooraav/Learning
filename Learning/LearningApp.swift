ea2dd1b0257fe7fdbad44293a7ebe7bbdc77298e
sooraav <purushothaman.sourav4@gmail.com>
2023-11-16 12:26:43 +0530

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Sourav on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
